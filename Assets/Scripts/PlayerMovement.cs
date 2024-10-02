d5f93cf35b5f076d32b3e4db57089ff6e0b6ef63
KILLGRIS\alexk <alex.kosco123@gmail.com>
2024-10-02 14:25:10 +0200

    git fixes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -29,6 +29,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //left and right movement
+        float horizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontal * playerSpeed, rb.velocity.y);
         
+        //jumping
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, playerJumpForce);
+                isJumping = true;
+            }
+        }
     }
 }
